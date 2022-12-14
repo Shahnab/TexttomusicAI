9a393575040756a4baeeb1f926ee5757af1fc419
SHAHNAB AHMED <31092371+Shahnab@users.noreply.github.com>
2022-12-15 00:01:43 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -7,7 +7,7 @@ st.sidebar.image("https://cdn-icons-png.flaticon.com/512/4430/4430494.png", widt
 st.sidebar.markdown("##### The app uses CLIP Interrogator to generate a text prompt which is then processed through Mubert text-to-music to generate melody from the input image")
 st.sidebar.subheader("About CLIP Interrogator Model")
 st.sidebar.markdown("##### The CLIP Interrogator is a prompt engineering tool that combines OpenAI's CLIP and Salesforce's BLIP to optimize text prompts to match a given image")
-st.sidebar.subheader("About Mulbert Text-to-Music Model")
+st.sidebar.subheader("About Mubert Text-to-Music Model")
 st.sidebar.markdown("##### Mubert is a startup that uses AI to generate music. The company has developed a text-to-music technology that can turn any text into a melody. Mubert is a leading provider of AI-powered sound design and composition tools")
 
 
